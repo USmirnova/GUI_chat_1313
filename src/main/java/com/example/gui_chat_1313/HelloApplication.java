0db0c2446b70a679d91e216b77d022db3fe42a9c
smirnova <dara071@yandex.ru>
2021-11-07 23:21:38 +0300

    Acceptance of online users by a serialized object

--- src/main/java/com/example/gui_chat_1313/HelloApplication.java
+++ src/main/java/com/example/gui_chat_1313/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.example.gui_chat_1313;
-// группа 1313, графический чат, лабораторная работа №10
-// меняю настройки под себя.
+// группа 1313, урок от 03.11.2021 д.з., графический чат финал, лабораторная работа №10.1
+// добавляем коллекцию онлайн пользователей в поле слева
+// принимаем и обрабатываем объеты (строку и/или коллекцию)
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
@@ -13,7 +14,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 300, 400); // создание объекта сцены с заданными параметрами ширины и высоты
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400); // создание объекта сцены с заданными параметрами ширины и высоты
         stage.setTitle("Клиент чата"); // заголовок окна
         stage.setScene(scene); // установить сцену?
         stage.show(); // показать сцену?
--- src/main/java/com/example/gui_chat_1313/HelloController.java
+++ src/main/java/com/example/gui_chat_1313/HelloController.java
@@ -9,11 +9,14 @@ import javafx.scene.control.TextField;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HelloController {
     DataOutputStream out; // Поток вывода выносим в эту область видимости, чтобы он был доступен из всех вложенных методов.
+    ArrayList<String> usersName = new ArrayList(); //Создадим здесь идентичную коллекцию как и на стороне сервера
     @FXML // аннотация
     Button connectBtn; // кнопка "Подключиться"
     @FXML
@@ -22,6 +25,8 @@ public class HelloController {
     TextField textField; // поле ввода данных
     @FXML
     TextArea textArea; // область текстовая
+    @FXML
+    TextArea onlineUsersTextArea; // для списка пользователей онлайн
     @FXML
     public void handlerSend() { // отправка сообщений (кнопка "отправить")
         String text = textField.getText(); // берем текст из поля ввода
@@ -41,16 +46,33 @@ public class HelloController {
             Socket socket = new Socket("127.0.0.1", 8179); // Проверить порт в консольной (серверной части). Должен совпадать
             System.out.println("Успешно подключен");
             out = new DataOutputStream(socket.getOutputStream());
-            DataInputStream in = new DataInputStream(socket.getInputStream());
+            //DataInputStream in = new DataInputStream(socket.getInputStream()); // заменяем на ObjectInputStream
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream()); // Принимать теперь будем объекты
             Thread thread = new Thread(new Runnable() { // отдельный поток для вывода серверных сообщений в textArea
                 @Override // например сообщение о присоединении новых пользователей и рассылки сообщений
                 public void run() {
                     try { // соединение в порядке
                         while (true){
-                            String response = in.readUTF(); // считываем данне из объекта ввода (сообщение с сервера)
-                            textArea.appendText(response+"\n"); // дописываем в textArea
+                            //String response = in.readUTF(); // считываем данне из объекта ввода (сообщение с сервера)
+                            // но теперь принимаем не строку, а объект и тут еще нужно выяснить, что это за объект (они же разные бывают)
+                            String response; // Присвоим значение, когда выясним, что присланный объет именно строка
+                            Object object = ois.readObject(); // тут снова требуется try/catch, т.к. исключения могут быть не только IOException, заменяем его на Exception
+                            System.out.println(object.getClass()); // метод getClass() покажет в консоли тип объекта (строка, массив, еще что-то) // class java.lang.String
+
+                            if(object.getClass().equals(usersName.getClass())) { // если пришел тип объекта ArrayList, то
+                                usersName = (ArrayList<String>) object; // этот объект надо преобразовать в коллекцию с помощью кастования - преобразования типов
+                                System.out.println("Коллекция"+usersName); // просто для наглядности
+                                onlineUsersTextArea.clear(); // очищаем, чтобы ничего не дозаписывалось при обновлении.
+                                for (String userName:usersName) {
+                                    onlineUsersTextArea.appendText(userName+"\n"); // дописываем в поле все имеющиеся имена
+                                }
+                            }
+                            else {
+                                response = object.toString(); // Преобразуем в строку
+                                textArea.appendText(response+"\n"); // дописываем в textArea
+                            }
                         }
-                    } catch (IOException e) {
+                    } catch (Exception e) { // IOException -> Exception
                         e.printStackTrace();
                         System.out.println("что-то с соединением");
                     }
